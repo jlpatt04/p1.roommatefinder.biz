2f535e25cc1079e1959af006bf0043d1286ade2b
jlpatt04 <jen.patten@gmail.com>
2013-09-13 13:35:23 -0400

    Added initial HTML5 syntax to index.php and created CSS file

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-echo "Welcome to PI";
-?>
+<!doctype html>
+<html lang ="en">
+<head>
+<title> Welcome to PI </title>
+<meta charset = "utf-8">
+<link rel="stylesheet" href="index.css">
+<!--[if IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+<header> 
+<h1>About Me</h1>
+<p> Who is Jen Patten? </p>
+</header>
+
+</html>
