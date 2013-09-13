cf785e90730c741e5be20cc788e19e5b9036faa5
jlpatt04 <jen.patten@gmail.com>
2013-09-13 17:27:46 -0400

    Added text to body

--- index.php
+++ index.php
@@ -14,4 +14,29 @@
 <p> Who is Jen Patten? </p>
 </header>
 
+<body>
+<section> 
+
+<p> My name is Jennifer Patten. Most people call me Jen, but my professional
+name is Jennifer. I'm from upstate New York. No, it's not even close to 
+NYC. Port Henry is a small town you have never heard of. </p>
+
+<p> I studied Economics and Spanish at St. Lawrence University. I wish I 
+would have taken a computer science course, but I didn't know very much about
+computer programming or knew anyone that knew how to program. Who would know
+8 years later I'm really enjoying programing? After a year of dabbling in control 
+structures and loops, I'm starting to understand how I can use programming 
+concepts to build something beautiful.</p>
+
+<p> What do I do for fun? Well I've been very busy lately with preparing
+for my classes and learning, but I'm looking forward to doing a little
+skiing this winter. I also enjoy being active and cooking different cuisines.
+I love anything spicy! </p>
+
+<p> Thanks for reading and I'm looking forward to building a "Dynamic Web 
+Application"! </p>
+
+</section>
+</body>
+
 </html>
