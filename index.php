b2a060ce1c81105703f34f249c2521db09de33eb
jlpatt04 <jen.patten@gmail.com>
2013-09-13 21:07:33 -0400

    minor text edits and added image

--- index.php
+++ index.php
@@ -11,27 +11,28 @@
 <body>
 <header> 
 <h1>About Me</h1>
-<p> Who is Jen Patten? </p>
+<p> Getting to know me a little better. </p>
+<img src="me.jpg" alt="Me" height="100" width="100">
 </header>
 
 <body>
 <section> 
 
-<p> My name is Jennifer Patten. Most people call me Jen, but my professional
-name is Jennifer. I'm from upstate New York. No, it's not even close to 
-NYC. Port Henry is a small town you have never heard of. </p>
+<p> My name is Jennifer Patten. Most people call me Jen.  I'm from upstate New York. 
+No, it's not even close to NYC. Port Henry is a small town you have never heard of. Situated in 
+in the Adirondack Mountains, it's a place of relaxation and nature.</p>
 
 <p> I studied Economics and Spanish at St. Lawrence University. I wish I 
 would have taken a computer science course, but I didn't know very much about
-computer programming or knew anyone that knew how to program. Who would know
-8 years later I'm really enjoying programing? After a year of dabbling in control 
-structures and loops, I'm starting to understand how I can use programming 
+computer programming at that time. I also didn't have one friend who studied
+programming. Who would think 8 years later I'm really enjoying programing? After six months 
+of dabbling in control structures and loops, I'm starting to understand how I can use programming 
 concepts to build something beautiful.</p>
 
 <p> What do I do for fun? Well I've been very busy lately with preparing
 for my classes and learning, but I'm looking forward to doing a little
-skiing this winter. I also enjoy being active and cooking different cuisines.
-I love anything spicy! </p>
+skiing this winter. I also enjoy being active and cooking different cuisines. My favorites are 
+Indian, Mexican, Ethiopian,  and Asian(Sushi). I love anything spicy and flavorful! </p>
 
 <p> Thanks for reading and I'm looking forward to building a "Dynamic Web 
 Application"! </p>
