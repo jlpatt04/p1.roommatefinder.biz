4faebdb92131a7bbef07aec56e763c2b145537d9
jlpatt04 <jen.patten@gmail.com>
2013-09-17 12:53:35 -0400

    Last minute text edits and fixed validation errors

--- index.php
+++ index.php
@@ -13,27 +13,29 @@
 <body>
 <header> 
 <h1>About Me</h1>
-<p> Getting to know me a little better. </p>
+<p> "Getting to know me a little better"</p>
 <img src="me.jpg" alt="Me" height="100" width="100">
 </header>
 
 <p>My name is Jennifer Patten. Most people call me Jen.  I'm from upstate New York. 
-No, it's not even close to NYC. Port Henry is a small town you have never heard of. Situated in 
-in the Adirondack Mountains, it's a place of relaxation and nature.</p>
+No, it's not even close to NYC. Port Henry is a small town located 6 hours north of NYC that
+you have never heard of. It is located in the Adirondack Mountains and on Lake Champlain. 
+Even though I grew up in rural upstate NY; I consider Boston my home. </p>
 
-<p>I studied Economics and Spanish at St. Lawrence University. I wish I 
+<p>In regards to my studies, I focused on Economics and Spanish at St. Lawrence University. I wish I 
 would have taken a computer science course, but I didn't know very much about
 computer programming at that time. I also didn't have one friend who studied
 programming. Who would think 8 years later I'm really enjoying programing? After six months 
-of dabbling in control structures and loops, I'm starting to understand how I can use programming 
-concepts to build something beautiful.</p>
+of dabbling in control structures,loops and variables, I'm starting to understand how I 
+can use programming concepts to build something beautiful.</p>
 
-<p>What do I do for fun? I enjoy reading and staying up to date with what's happening
-around the globe. I recently finished "Dreams from my Father" and "A Reason to Believe". Obama and 
-Deval Patrick's biographies inspire me not only to do great things in my life, but to give back to society
-and make an impact in my corner of the world. On a lighter note, I really enjoy skiing in the winter. It's
-such an exhilarating sport. Additionally, I enjoy being active and cooking different cuisines. My favorites
-are Indian, Mexican, Ethiopian, and Asian(Sushi). I love anything spicy and flavorful! </p>
+<p>What do I do for fun? I enjoy reading and staying up to date with current events. AKA I am
+an avid tweeter and I don't use facebook. I recently finished "Dreams from my Father" and "A 
+Reason to Believe". Barrack Obama and Deval Patrick's biographies inspire me not only to do 
+great things in my life, but to give back to society and make an impact in my corner of the world.
+On a lighter note, I really enjoy skiing in the winter. It's such an exhilarating sport. Additionally,
+I enjoy cooking different cuisines. My favorites are Indian, Mexican, Ethiopian, and Asian(Sushi).
+I love anything spicy and/or flavorful! </p>
 
 <p>Thanks for reading and I'm looking forward to building a "Dynamic Web 
 Application"! </p>
